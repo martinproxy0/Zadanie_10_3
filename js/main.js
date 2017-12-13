0ee57b1b3da03d4b5932381538b7b60f2d0815a3
Marcin Sornek <marcinsornek@gmail.com>
2017-12-13 08:26:47 +0100

    Add code for exercise 10.3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 // globals
-var carouselList = $("#carousel ul");
+var carouselList = $('#carousel ul');
+var sliderNavPoints = $('.slider-nav-point');
+var sliderNavPointsArray = [];
+    sliderNavPoints.each(function (index) {
+        sliderNavPointsArray[index] = sliderNavPoints[index].outerHTML;
+      });
+var sliderNav = $('#slider-nav');
+var intervalId = 0;
+
 
 $(function() {
     initialize();
@@ -10,19 +18,51 @@ function initialize() {
     $('#btn-left').click(function(e) {
         e.preventDefault();
         carouselList.stop(true);
-        moveSlideLeft();
-        carouselList.css({marginLeft:-400});
-        carouselList.animate({'marginLeft':0},1000);
+        shiftSlide(400,moveSlideLeft);
     });
-    $("#btn-right").click(function(e) {
+    $('#btn-right').click(function(e) {
         event.preventDefault();
-        carouselList.stop(true);
-        carouselList.animate({'marginLeft':-400},1000,moveSlideRight);
+        carouselList.stop(true,true);
+        shiftSlide(-400,moveSlideRight);
     });
+    addSliderNavListener();
+    $('#carousel').mouseenter(function () { clearInterval(intervalId); });
+    $('#carousel').mouseleave(function () { runCarousel(); });
+}
+
+function addSliderNavListener () {
+    $('.slider-nav-point').click(function() {changeSlideFromNav($(this));});
+}
+
+function changeSlideFromNav(e) {
+    var chosen = e.index();
+    var active = $('.slider-nav-point i').index($('.fa-circle'));
+    var margineValue = 0;
+
+    if (chosen > -1) {
+
+        if (active < chosen) {
+            for (let i = 0; i < chosen - active ; i++) {
+                moveSlideRight();       
+            }
+            margineValue = 400*(chosen - active);
+            carouselList.css({'marginLeft':margineValue});
+            shiftSlide(0);
+        } 
+        if (active > chosen) {
+            for (let i = 0; i < (active - chosen); i++) {
+                moveSlideLeft();
+            }
+            margineValue = -400*(active - chosen);
+            carouselList.css({'marginLeft':margineValue});
+            shiftSlide(0);
+
+        }
+    }
 }
 
 function runCarousel() { 
-    setInterval(function(){shiftSlide(-400,moveSlideRight);}, 4000);
+    //intervalId = setInterval(function(){shiftSlide(-400,moveSlideRight);}, 4000);
 }
 
 function shiftSlide(direction,action) {
@@ -34,11 +74,30 @@ function moveSlideRight() {
     var firstItem = carouselList.find("li:first");
     var lastItem = carouselList.find("li:last");
     lastItem.after(firstItem);
-    carouselList.css({'marginLeft':0});
+    resetCarouselMargin();
+    sliderNavPointRight();
 }
 
 function moveSlideLeft() {
     var firstItem = carouselList.find("li:first");
     var lastItem = carouselList.find("li:last");
     firstItem.before(lastItem);
+    resetCarouselMargin();
+    sliderNavPointLeft();
+}
+
+function resetCarouselMargin() {
+    carouselList.css({'marginLeft':0});
+}
+
+function sliderNavPointLeft() {
+    sliderNavPointsArray.push(sliderNavPointsArray.shift());
+    sliderNav.html(sliderNavPointsArray);
+    addSliderNavListener();  
+}
+
+function sliderNavPointRight() {
+    sliderNavPointsArray.unshift(sliderNavPointsArray.pop());
+    sliderNav.html(sliderNavPointsArray);
+    addSliderNavListener();
 }
